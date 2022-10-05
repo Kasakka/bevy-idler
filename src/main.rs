a766b14eb0a61e4dab401816123d281974363837
Kasakka <henri.matero@gmail.com>
2022-10-05 21:42:15 +0300

    Added generator lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Building;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Price(f32);
+
+#[derive(Component)]
+struct UpgradeLevel(i32);
+
+#[derive(Component)]
+struct Factor(f32);
+
+fn add_building(mut commands: Commands) {
+    commands.spawn()
+        .insert(Building)
+        .insert(Name("Kultakaivos".to_string()));
+}
+
+fn print_building(query: Query<&Name, With<Building>>) {
+    for name in query.iter() {
+        println!("Rakennus: {}", name.0);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(add_building)
+        .add_system(print_building)
+        .run();
 }
